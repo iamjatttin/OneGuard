a88b273d52f8f7a5f9456fd60094d86ede8b1629
Jatin Choudhary <jatinranjit@gmail.com>
2022-06-28 23:34:15 +0530

    Layout Fixing V1

--- index.php
+++ index.php
@@ -1,36 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css"
-    />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" />
     <link rel="stylesheet" href="assets/css/dataTables.bootstrap5.min.css" />
     <link rel="stylesheet" href="assets/css/style.css" />
     <link rel="icon" type="image/x-icon" href="assets/images/logo.png">
     <title>OneGuard | Dashboard</title>
-  </head>
-  <body>
+</head>
+
+<body>
     <!-- header include -->
     <?php 
       include './partials/header.php';
     ?>
     <!-- header include -->
+    <!-- Main COntent -->
     <main class="mt-5 pt-3">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-12">
-            <h4>Team</h4>
-          </div>
-        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12 mt-5 text-center ms-auto">
+                    <h2>Here is where all the main content will be added</h2>
+                </div>
+            </div>
 
-      </div>
+        </div>
     </main>
-    
+    <!-- Main Content end -->
 
 
     <script src="./assets/js/bootstrap.bundle.min.js"></script>
@@ -39,5 +39,6 @@
     <script src="./assets/js/jquery.dataTables.min.js"></script>
     <script src="./assets/js/dataTables.bootstrap5.min.js"></script>
     <script src="./assets/js/script.js"></script>
-  </body>
+</body>
+
 </html>
